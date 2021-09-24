7513a8a0ab47755c041579505a964ea41b1bea52
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-09-24 11:10:18 -0500

    added .focus() to input search

--- app.js
+++ app.js
@@ -33,11 +33,14 @@ element.addEventListener("click", onTabClick, false);
 
 const trigger_search = document.querySelector(".trigger_search");
 const input = document.querySelector(".input");
+const inputSearchFocus = document.getElementById("input");
 
 trigger_search.addEventListener("click", () => {
   if (!input.classList.contains("input-open")) {
     input.classList.add("input-open");
+
     trigger_search.innerHTML = "<i class='fas fa-times'>  </i>";
+    inputSearchFocus.focus();
   } else {
     input.classList.remove("input-open");
     trigger_search.innerHTML = "<i class='fas fa-search'>  </i>";
