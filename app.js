90783a139cf1f735df8a40f4a3dde6744f5979d9
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-09-27 09:59:24 -0500

    fixed the search and typeahead functionality

--- app.js
+++ app.js
@@ -1,174 +1,180 @@
-/*********************************************
- *
- * Inner School tab
- *
- * ********************************************/
+document.addEventListener("DOMContentLoaded", () => {
+  /*********************************************
+   *
+   * Inner School tab
+   *
+   * ********************************************/
 
-function onTabClick(event) {
-  let activeTabs = document.querySelectorAll(".active");
+  function onTabClick(event) {
+    let activeTabs = document.querySelectorAll(".active");
 
-  // deactivate existing active tab and panel
-  // for( let i = 0; i < activeTabs.length; i++) {
-  //   activeTabs[i].className = activeTabs[i].className.replace('active', '');
-  // }
+    // deactivate existing active tab and panel
+    // for( let i = 0; i < activeTabs.length; i++) {
+    //   activeTabs[i].className = activeTabs[i].className.replace('active', '');
+    // }
 
-  activeTabs.forEach(function (tab) {
-    tab.className = tab.className.replace("active", "");
+    activeTabs.forEach(function (tab) {
+      tab.className = tab.className.replace("active", "");
+    });
+
+    // activate new tab and panel
+    event.target.parentElement.className += " active";
+    document.getElementById(event.target.href.split("#")[1]).className +=
+      " active";
+  }
+
+  const element = document.getElementById("nav-tab");
+  element.addEventListener("click", onTabClick, false);
+
+  /*********************************************
+   *
+   * Search Icons
+   *
+   * ********************************************/
+
+  const trigger_search = document.querySelector(".trigger_search");
+  const input = document.querySelector(".input");
+  const inputSearchFocus = document.getElementById("input");
+  const search__results_container = document.getElementById(
+    "search__results_container"
+  );
+
+  trigger_search.addEventListener("click", () => {
+    if (!input.classList.contains("input-open")) {
+      input.classList.add("input-open");
+      trigger_search.innerHTML = `<i aria-label="Close search " tabindex="0" class='fas fa-times icon'>  </i>`;
+      inputSearchFocus.focus();
+    } else {
+      input.classList.remove("input-open");
+      trigger_search.innerHTML = `<i aria-label="Click to open the search and just tart typing" tabindex="0" class='fas fa-search icon'>  </i>`;
+    }
   });
 
-  // activate new tab and panel
-  event.target.parentElement.className += " active";
-  document.getElementById(event.target.href.split("#")[1]).className +=
-    " active";
-}
+  /*********************************************
+   *
+   * On click event to help keyboard only users
+   *
+   * ********************************************/
 
-const element = document.getElementById("nav-tab");
-element.addEventListener("click", onTabClick, false);
+  const btnNews = document.getElementById("btn__news");
+  const btnAbout = document.getElementById("btn__about");
+  const btnSchools = document.getElementById("btn__schools");
+  const btnEmployment = document.getElementById("btn__employment");
+  const btnClose = document.getElementById("btn__close");
+  const btnMobileOpen = document.getElementById("btn__mobile_open");
+  const btnMobileClose = document.getElementById("btn__mobile_close");
 
-/*********************************************
- *
- * Search Icons
- *
- * ********************************************/
+  let navOpen = false;
 
-const trigger_search = document.querySelector(".trigger_search");
-const input = document.querySelector(".input");
-const inputSearchFocus = document.getElementById("input");
+  const onClick_news = document.getElementById("display__onClick_news");
+  const onClick_about = document.getElementById("display__onClick_about");
+  const onClick_schools = document.getElementById("display__onClick_schools");
+  const onClick_employment = document.getElementById(
+    "display__onClick_employment"
+  );
 
-trigger_search.addEventListener("click", () => {
-  if (!input.classList.contains("input-open")) {
-    input.classList.add("input-open");
+  const classItem_news = "show";
+  const classItem_about = "show_about";
+  const classItem_schools = "show_schools";
+  const classItem_employment = "show_employment";
 
-    trigger_search.innerHTML = `<i aria-label="Close search " tabindex="0" class='fas fa-times icon'>  </i>`;
-    inputSearchFocus.focus();
-  } else {
-    input.classList.remove("input-open");
-    trigger_search.innerHTML = `<i aria-label="Click to open the search and just tart typing" tabindex="0" class='fas fa-search icon'>  </i>`;
+  function showMenuItem(containerID, classItem) {
+    console.log(element);
+    containerID.classList.toggle(`${classItem}`);
   }
-});
+  function hideMenuItem(containerID, classItem) {
+    containerID.classList.remove(`${classItem}`);
+  }
 
-/*********************************************
- *
- * On click event to help keyboard only users
- *
- * ********************************************/
-
-const btnNews = document.getElementById("btn__news");
-const btnAbout = document.getElementById("btn__about");
-const btnSchools = document.getElementById("btn__schools");
-const btnEmployment = document.getElementById("btn__employment");
-const btnClose = document.getElementById("btn__close");
-const btnMobileOpen = document.getElementById("btn__mobile_open");
-const btnMobileClose = document.getElementById("btn__mobile_close");
-
-let navOpen = false;
-
-const onClick_news = document.getElementById("display__onClick_news");
-const onClick_about = document.getElementById("display__onClick_about");
-const onClick_schools = document.getElementById("display__onClick_schools");
-const onClick_employment = document.getElementById(
-  "display__onClick_employment"
-);
-
-const classItem_news = "show";
-const classItem_about = "show_about";
-const classItem_schools = "show_schools";
-const classItem_employment = "show_employment";
-
-function showMenuItem(containerID, classItem) {
-  console.log(element);
-  containerID.classList.toggle(`${classItem}`);
-}
-function hideMenuItem(containerID, classItem) {
-  containerID.classList.remove(`${classItem}`);
-}
-
-btnNews.addEventListener("click", () => {
-  hideMenuItem(onClick_about, classItem_about);
-  hideMenuItem(onClick_schools, classItem_schools);
-  hideMenuItem(onClick_employment, classItem_employment);
-  showMenuItem(onClick_news, classItem_news);
-  //Click outside the element close the modal
-  window.onclick = function (event) {
-    if (!event.target.matches("#btn__news")) {
-      var dropdowns = document.getElementsByClassName("display__onClick");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-          openDropdown.classList.remove("show");
+  btnNews.addEventListener("click", () => {
+    hideMenuItem(onClick_about, classItem_about);
+    hideMenuItem(onClick_schools, classItem_schools);
+    hideMenuItem(onClick_employment, classItem_employment);
+    showMenuItem(onClick_news, classItem_news);
+    //Click outside the element close the modal
+    window.onclick = function (event) {
+      if (!event.target.matches("#btn__news")) {
+        var dropdowns = document.getElementsByClassName("display__onClick");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+          }
         }
       }
-    }
-  };
-});
+    };
+  });
 
-btnAbout.addEventListener("click", () => {
-  hideMenuItem(onClick_news, classItem_news);
-  hideMenuItem(onClick_schools, classItem_schools);
-  hideMenuItem(onClick_employment, classItem_employment);
+  btnAbout.addEventListener("click", () => {
+    hideMenuItem(onClick_news, classItem_news);
+    hideMenuItem(onClick_schools, classItem_schools);
+    hideMenuItem(onClick_employment, classItem_employment);
 
-  showMenuItem(onClick_about, classItem_about);
-  //Click outside the element close the modal
-  window.onclick = function (event) {
-    if (!event.target.matches("#btn__about")) {
-      var dropdowns = document.getElementsByClassName("display__onClick_about");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show_about")) {
-          openDropdown.classList.remove("show_about");
+    showMenuItem(onClick_about, classItem_about);
+    //Click outside the element close the modal
+    window.onclick = function (event) {
+      if (!event.target.matches("#btn__about")) {
+        var dropdowns = document.getElementsByClassName(
+          "display__onClick_about"
+        );
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show_about")) {
+            openDropdown.classList.remove("show_about");
+          }
         }
       }
-    }
-  };
-});
+    };
+  });
 
-btnSchools.addEventListener("click", () => {
-  hideMenuItem(onClick_news, classItem_news);
-  hideMenuItem(onClick_about, classItem_about);
-  hideMenuItem(onClick_employment, classItem_employment);
+  btnSchools.addEventListener("click", () => {
+    hideMenuItem(onClick_news, classItem_news);
+    hideMenuItem(onClick_about, classItem_about);
+    hideMenuItem(onClick_employment, classItem_employment);
 
-  showMenuItem(onClick_schools, classItem_schools);
-});
+    showMenuItem(onClick_schools, classItem_schools);
+  });
 
-btnEmployment.addEventListener("click", () => {
-  hideMenuItem(onClick_news, classItem_news);
-  hideMenuItem(onClick_about, classItem_about);
-  hideMenuItem(onClick_schools, classItem_schools);
-  showMenuItem(onClick_employment, classItem_employment);
-  //Click outside the element close the modal
-  window.onclick = function (event) {
-    if (!event.target.matches("#btn__employment")) {
-      var dropdowns = document.getElementsByClassName(
-        "display__onClick_employment"
-      );
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show_employment")) {
-          openDropdown.classList.remove("show_employment");
+  btnEmployment.addEventListener("click", () => {
+    hideMenuItem(onClick_news, classItem_news);
+    hideMenuItem(onClick_about, classItem_about);
+    hideMenuItem(onClick_schools, classItem_schools);
+    showMenuItem(onClick_employment, classItem_employment);
+    //Click outside the element close the modal
+    window.onclick = function (event) {
+      if (!event.target.matches("#btn__employment")) {
+        var dropdowns = document.getElementsByClassName(
+          "display__onClick_employment"
+        );
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show_employment")) {
+            openDropdown.classList.remove("show_employment");
+          }
         }
       }
-    }
-  };
-});
+    };
+  });
 
-btnClose.addEventListener("click", () => {
-  hideMenuItem(onClick_schools, classItem_schools);
-});
+  btnClose.addEventListener("click", () => {
+    hideMenuItem(onClick_schools, classItem_schools);
+  });
 
-/*******************************************************
+  /*******************************************************
 =====================================
 ==>        Mobile
 =====================================
 *******************************************************/
 
-btnMobileOpen.addEventListener("click", () => {
-  document.getElementById("nav").style.left = "0%";
-});
+  btnMobileOpen.addEventListener("click", () => {
+    document.getElementById("nav").style.left = "0%";
+  });
 
-btnMobileClose.addEventListener("click", () => {
-  console.log("mobileMenu btn work");
-  document.getElementById("nav").style.left = "150%";
+  btnMobileClose.addEventListener("click", () => {
+    console.log("mobileMenu btn work");
+    document.getElementById("nav").style.left = "150%";
+  });
 });
--- search.js
+++ search.js
@@ -1,8 +1,49 @@
 const search__results = document.getElementById("search__results");
 const searchBar = document.getElementById("input");
 
+const searchStates = async (searchText) => {
+  const res = await fetch(
+    "https://www.friscoisd.org/searchData/searchTerms.json"
+  );
+  const states = await res.json();
+
+  // Get match to current text input
+
+  let matches = states.filter((state) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.t.match(regex) || state.u.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    search__results.innerHTML = "";
+  }
+  console.log(matches);
+
+  displayDataHTML(matches);
+};
+
+// show data to HTML page
+const displayDataHTML = (matches) => {
+  if (matches.length > 0) {
+    const url = `https://www.friscoisd.org/sc/`;
+    const htmlData = matches
+      .map((match) => {
+        return `
+            <li class="character">
+                <a class="search__link" href="${url}${match.u}" >${match.t}</a>
+            </li>
+        `;
+      })
+      .join("");
+    search__results.innerHTML = htmlData;
+  }
+};
+
+searchBar.addEventListener("input", () => searchStates(searchBar.value));
+
 /* get ref of the retrieved  data made globally available
-to be used anywhere in the code.*/
+to be used anywhere in the code.
 let hpCharacters = [];
 
 const loadCharacters = async () => {
@@ -35,6 +76,7 @@ const displayCharacters = (characters) => {
   if (searchBar.length === 0) {
     search__results.innerHTML = NO_DATA_FOUND;
   } else {
+    search__results_container.style.display === "block";
     search__results.innerHTML = htmlString;
   }
 };
@@ -61,3 +103,4 @@ loadCharacters();
 if (searchBar.length === 0) {
   search__results.innerHTML = "";
 }
+*/
