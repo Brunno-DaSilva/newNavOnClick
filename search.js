e2a58b312fba916e9bae1d3c1490b7468980645f
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-09-27 10:05:29 -0500

    removed ^ char from regex to enable all searches matching the search input

--- search.js
+++ search.js
@@ -10,7 +10,7 @@ const searchStates = async (searchText) => {
   // Get match to current text input
 
   let matches = states.filter((state) => {
-    const regex = new RegExp(`^${searchText}`, "gi");
+    const regex = new RegExp(`${searchText}`, "gi");
     return state.t.match(regex) || state.u.match(regex);
   });
 
@@ -41,3 +41,66 @@ const displayDataHTML = (matches) => {
 };
 
 searchBar.addEventListener("input", () => searchStates(searchBar.value));
+
+/* get ref of the retrieved  data made globally available
+to be used anywhere in the code.
+let hpCharacters = [];
+
+const loadCharacters = async () => {
+  try {
+    const res = await fetch(
+      "https://www.friscoisd.org/searchData/searchTerms.json"
+    );
+    hpCharacters = await res.json();
+
+    displayCharacters(hpCharacters);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayCharacters = (characters) => {
+  const url = `https://www.friscoisd.org/sc/`;
+  const NO_DATA_FOUND = `<p>No Data Found </p>`;
+
+  const htmlString = characters
+    .map((character) => {
+      return `
+            <li class="character">
+                <a class="search__link" href="${url}${character.u}" >${character.t}</a>
+            </li>
+        `;
+    })
+    .join("");
+
+  if (searchBar.length === 0) {
+    search__results.innerHTML = NO_DATA_FOUND;
+  } else {
+    search__results_container.style.display === "block";
+    search__results.innerHTML = htmlString;
+  }
+};
+
+searchBar.addEventListener("keyup", (e) => {
+  const userInput = e.target.value.toLowerCase();
+  console.log(userInput);
+
+  const filterChar = hpCharacters.filter((character) => {
+    return (
+      character.t.toLowerCase().includes(userInput) ||
+      character.u.toLowerCase().includes(userInput)
+    );
+  });
+  displayCharacters(filterChar);
+
+  // onKeyup add a class of isActive
+  // to searchItemsContainer once searchbar
+  // is clear add a class of display none
+});
+
+loadCharacters();
+
+if (searchBar.length === 0) {
+  search__results.innerHTML = "";
+}
+*/
